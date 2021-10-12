470807f796b51a275b1af91239d2e05b0bedd85e
Pierce O'Neill <pierceoneill@Pierces-MacBook-Pro.local>
2021-10-12 19:40:58 +0100

    updated env file locally

--- realtors/models.py
+++ realtors/models.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 class Realtor(models.Model):
   name = models.CharField(max_length=200)
-  image = models.ImageField(upload_to='images')
+  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
   description = models.TextField(blank=True)
   phone = models.CharField(max_length=20)
   email = models.CharField(max_length=50)
   is_mvp = models.BooleanField(default=False)
   hire_date = models.DateTimeField(default=datetime.now, blank=True)
   def __str__(self):
-        return self.name
+    return self.name
