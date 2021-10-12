613fe9f17ef150ffe3f25aa3f167cadaf11570b9
Pierce O'Neill <pierceoneill@Pierces-MacBook-Pro.local>
2021-10-12 17:00:49 +0100

    updated env file locally

--- ponredb/settings.py
+++ ponredb/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 from django.contrib.messages import constants as messages
 import os
-import env
 import dj_database_url
 from datetime import timedelta
 from pathlib import Path
